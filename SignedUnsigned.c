ffb1c2cc829ba72d675277e8418d81f627c3d8b2
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-04-26 12:56:59 +0530

    Add files via upload

---
+++ SignedUnsigned.c
@@ -0,0 +1,21 @@
+// const is a qualifies
+
+# include <stdio.h>
+
+int main(){
+
+    int i = 11;                   // signed
+    int j = -11;                  // signed
+
+    unsigned int k = 11;          // Unsigned
+
+    signed int a = 11;            // signed
+    signed int b = -11;           // signed
+
+    // unsigned int l = -11;   
+
+    // printf("%d\n",l);
+
+    return 0;
+}
+
