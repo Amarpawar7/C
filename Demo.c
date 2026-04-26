fe46ef8ec338215455b67411b2adad9b2870de53
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-04-26 12:55:34 +0530

    Add files via upload

---
+++ Demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>           
+// # : preprocessor directive symbol ,every line starting with # is handled by the preprocessor     
+// include : this indicates preprocessor to load the desired header file in the application program
+// stdio.h : standard Input/Output header file
+
+int main(){
+// int : Datatype which indicates the written value of the main function
+// mian() : entry point functions from where the execution of application starts
+
+    printf("Hello World...\n");            
+    // \n : escape sequence that indicates the new line character
+    // semicolun indicates the end of a executable line
+
+    return 0;
+    // return : it just returns the output
+    // 0 : It indicates the Operating System that the process gets terminated sucessfully
+    // if its -1 : it indicates abnormal termination      
+
+}
+   
+
