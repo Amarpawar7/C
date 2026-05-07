a5508802c9fedea85c4908edfbb2448c78058730
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-07 22:31:44 +0530

    Add files via upload

---
+++ StringLengthUserDefined.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+// Call by address function
+int strlenX(char *ptr){
+
+    int i = 0;
+
+    while(*ptr != '\0'){
+        i++;
+        ptr++;
+    }
+
+    return i;
+}
+
+int main(){
+    
+    char str[] = "Ganesh";
+    int ret = 0;
+    ret = strlenX(str);
+
+    printf("String length is : %d\n",ret);
+
+    return 0;
+}
